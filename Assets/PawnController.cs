ab3c3206a57be2e0c135dd22d91d587698ee5945
Justine Tristan <103434279+LeTristanJ13@users.noreply.github.com>
2024-10-22 12:01:44 +0200

    Update

--- Assets/PawnController.cs
+++ Assets/PawnController.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Personnage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject target;
 
-    // Update is called once per frame
-    void Update()
+    NavMeshAgent agent;
+    Animator animator;
+    float SpeedRef;
+
+
+    private void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.transform.position);
+        agent.speed=5;
+        animator = GetComponent<Animator>();
+    }
+     void Update()
+    {   if(animator != null)
+        {
+            SpeedRef= agent.velocity.magnitude;
+            animator.SetFloat("Speed", SpeedRef);
+        }
+    
+            
     }
 }
+
+
+   
